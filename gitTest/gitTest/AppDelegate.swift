cb80f715b8cfdc680ba98a6cebea40155d77e10c
Alexandra Georgieva <alexandra.georgieva@trading212.com>
2015-05-14 14:49:47 +0300

    commit 0 1

--- gitTest/gitTest/AppDelegate.swift
+++ gitTest/gitTest/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //commit 0 1
+        //commit 0 1
+        
         return true
     }
 
