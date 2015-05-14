ec8d2267dd332d2686b3fd960387845a2d3f0bc1
Alexandra Georgieva <alexandra.georgieva@trading212.com>
2015-05-14 15:07:45 +0300

    commit 0 2

--- gitTest/gitTest/AppDelegate.swift
+++ gitTest/gitTest/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        //commit 1
+        //commit 1
+        //commit 1
         //commit 0 1
         //commit 0 1
         
@@ -34,6 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //commit 2
         //commit 2
         
+
+        //commit 0 2
+        //commit 0 2
         
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
