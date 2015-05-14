fec7ba325a50ecc04d334ffca44bb294fd8b4c73
Alexandra Georgieva <alexandra.georgieva@trading212.com>
2015-05-14 15:27:11 +0300

    commit 0 3

--- gitTest/gitTest/AppDelegate.swift
+++ gitTest/gitTest/AppDelegate.swift
@@ -41,6 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //commit 0 2
         //commit 0 2
         
+        //commit 0 3
+        //commit 0 3
+        //commit 0 3
+        
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
